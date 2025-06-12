130885bc83822bb02649289a9d00a43058092f33
S.Bera <147972880+sooraj357@users.noreply.github.com>
2025-06-12 12:36:45 +0530

    Create Indices.py
    update

---
+++ Indices.py
@@ -0,0 +1,15 @@
+nums = [2, 7, 11, 15]
+target = 9
+
+def two_sum(nums, target):
+    lookup = {}
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in lookup:
+            return [lookup[complement], index]
+        lookup[num] = index
+    return None
+
+# Call the function *after* defining it
+result = two_sum(nums, target)
+print("Indices:", result)
